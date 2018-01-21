e6bca342684efae5eb3397c20e8db8133f4b1eca
Aaron Qubit <like.xu@intel.com>
2018-01-21 19:48:19 +0800

    init from v4.10

---
+++ apply.sh
@@ -0,0 +1,28 @@
+CICD=/Volumes/hardcore/git-clone-repo/kenerl_hack_include_changelog
+SRC=/Volumes/hardcore/git-clone-repo/linux
+STRUCTURE=feature-structure.txt
+
+cd $SRC
+find . -name \*.h -print > $CICD/feature-structure.txt
+cd $CICD
+
+sed 's/^..//' feature-structure.txt > tmp.out
+mv tmp.out feature-structure.txt
+
+declare -a codes=()
+
+cd $SRC
+echo -e "\n[START] synchronizing files...\n"
+cat $CICD/$STRUCTURE | while read var
+do
+    if [ ! -f "$CICD/${var}" ]; then
+        echo "acquire ${var}"
+        /usr/local/opt/coreutils/libexec/gnubin/cp --parents ${var} $CICD/
+    else
+        echo "update ${var}"
+        /usr/local/opt/coreutils/libexec/gnubin/cp $CICD/${var} $SRC/${var}
+    fi
+done
+cd $CICD
+
+echo -e "\n[DONE] synchronizing files \n"
---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#1 SMP Thu Jan 18 19:38:32 CST 2018"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "macos-ubuntu"
+#define LINUX_COMPILER "gcc version 7.2.0 (Ubuntu 7.2.0-8ubuntu3)"
